da9b689a23294c6346b1383c4dd126f7e09ecb7b
Niklas Sauer <nik.sauer@me.com>
2018-03-04 13:09:32 +0100

    adjust package to properly export targets, move request models to subdirectory

--- Package.swift
+++ Package.swift
@@ -1,7 +1,14 @@
 // swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
-    name: "RoadChatKit"
+    name: "RoadChatKit",
+    products: [
+        .library(name: "RoadChatKit", targets: ["RoadChatKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "RoadChatKit", dependencies: [], path: "./Sources"),
+        .testTarget(name: "RoadChatKitTests", dependencies: ["RoadChatKit"])
+    ]
 )
