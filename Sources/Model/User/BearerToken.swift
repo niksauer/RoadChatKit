56f031ad3001f26e914a34d1c78d2d73fa98eadc
Niklas Sauer <nik.sauer@me.com>
2018-03-08 11:29:17 +0100

    add userID to PublicBearerToken

--- Sources/Model/User/BearerToken.swift
+++ Sources/Model/User/BearerToken.swift
@@ -24,10 +24,12 @@ public extension BearerToken {
     }
     
     public struct PublicBearerToken: Codable {
-        public let token: String
+        public let userID: Int
+	public let token: String
         
         public init(token: BearerToken) {
-            self.token = token.token
+            self.userID = token.userID
+	    self.token = token.token
         }
     }
 }
